049d6591406337de08a0243d85dd64c40b1bea78
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-06-29 13:53:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
         st.write("Filtered Frogs:")
         for frog in filtered_frogs:
             #st.write(frog)
-            url = "https://ordiscan.com/content/"+frog["inscription_id"]+".webp"
+            url = "https://ordiscan.com/content/"+frog["inscription_id"]
             width = 576/4  # 图片宽度（以像素为单位）
             height = 576/4  # 图片高度（以像素为单位）
             st.image(url, caption=frog["item_name"], width=width, height=height)
