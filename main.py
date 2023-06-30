fb9aaab1f96837cef809c1be9591b83a840bb218
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-06-30 14:34:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ with open('bitcoin_frogs_items.json') as f:
 filtered_frogs = []
 
 def main():    
-    st.write("Floor Price (ME) : ",rounded_floor_price)
+    st.write("Floor Price (ME) : ",rounded_floor_price,"Floor Price (ME) : ",rounded_floor_price)
     st.sidebar.image("https://cdn.discordapp.com/attachments/1117712065293987840/1124212987243278356/rpbp.png", use_column_width=True)
     st.sidebar.title("Bitcoin Frogs")
     st.sidebar.title("Traits Filter")
