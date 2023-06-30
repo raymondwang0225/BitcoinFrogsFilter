b86341131ebc1dedde73bb6d3a3f0f76e3582ff2
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-06-30 14:52:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ with open('bitcoin_frogs_items.json') as f:
 filtered_frogs = []
 
 def main():    
-    st.write("Floor Price (ME) : ",rounded_floor_price," Owners : ",owners," Total Listed : ",totalListed," Total Volume : ",rounded_totalVolume)
+    st.write("Floor Price : ",rounded_floor_price," Owners : ",owners," Total Listed : ",totalListed," Total Volume : ",rounded_totalVolume," (Magic Eden)")
     #st.write("<p style='margin-top: 20px; margin-bottom: 20px'></p>", unsafe_allow_html=True)
    
     st.sidebar.image("https://cdn.discordapp.com/attachments/1117712065293987840/1124212987243278356/rpbp.png", use_column_width=True)
