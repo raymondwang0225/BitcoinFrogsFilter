1bfeb9e7366c82786137449fa517428ef18c4e1c
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-06-30 16:34:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,9 @@ def main():
         for i, frog in enumerate(filtered_frogs):
             with cols[i % col_width]:
                 # 将变量传递给HTML代码
-                formatted_link_html = link_html.format(caption=frog["item_name"], url=frog["me_link"])
+                _caption = frog["item_name"]
+                _link_url = frog["me_link"]
+                formatted_link_html = link_html.format(caption=_caption, url=_link_url)
                 st.image(frog["image_url"], caption=formatted_link_html,width=576/4)
                  
                 
