c61df3b094050ad9aa3eab25a952ba631b11d55c
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-01 13:43:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ st.set_page_config(layout="wide")
 
 def main():
     st.write("[Magic Eden]")
-    st.write("Floor Price : ",rounded_floor_price," Owners : ",owners," Total Listed : ",totalListed," Total Volume : ",rounded_totalVolume)
+    #st.write("Floor Price : ",rounded_floor_price," Owners : ",owners," Total Listed : ",totalListed," Total Volume : ",rounded_totalVolume)
     col1, col2, col3 ,col4= st.columns(4)
 
-    col1.metric("Floor Price", rounded_floor_price, "--") 
-    col2.metric("Owners", owners, "--") 
-    col3.metric("Total Listed", totalListed, "--") 
-    col4.metric("Total Volume", rounded_totalVolume, "--") 
+    col1.metric("Floor Price", rounded_floor_price) 
+    col2.metric("Owners", owners) 
+    col3.metric("Total Listed", totalListed) 
+    col4.metric("Total Volume", rounded_totalVolume) 
    
     st.sidebar.image("https://cdn.discordapp.com/attachments/1117712065293987840/1124212987243278356/rpbp.png", use_column_width=True)
     #st.sidebar.title("Bitcoin Frogs")
