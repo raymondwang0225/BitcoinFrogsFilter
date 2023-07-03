bb7db5e446e5d5a7c28f5d15ba228c0dabbeb742
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-03 17:31:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def main():
     
     tab1, tab2, tab3, tab4 = st.tabs(["Floor Price", "Owners","Total Listed","Total Volume"])
     with tab1:
-        st.line_chart(fpcsv[["timestamp", "floor_price"]], x = 'timestamp',height =20)
+        st.line_chart(fpcsv[["timestamp", "floor_price"]], x = 'timestamp')
     with tab2:
         st.line_chart(fpcsv[["timestamp", "owners"]], x = 'timestamp')
     with tab3:
