8d0c9e57ffc4eb69f3fb1b9ccfbd51545406d5ac
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-03 11:34:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def main():
     
     st.markdown("<hr/>", unsafe_allow_html = True)
 
-    fpcsv = pd.read_csv('floor_price.csv', parse_dates=['timestamp'])
-    st.line_chart(fpcsv["floor_price"], x = 'timestamp')
+    fpcsv = pd.read_csv('floor_price.csv', header=1, usecols=["timestamp", "floor_price"], parse_dates=['timestamp'])
+    st.line_chart(fpcsv, x = 'timestamp')
     
     st.sidebar.image("https://cdn.discordapp.com/attachments/1117712065293987840/1124212987243278356/rpbp.png", use_column_width=True)
     #st.sidebar.title("Bitcoin Frogs")
