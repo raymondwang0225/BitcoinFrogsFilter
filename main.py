fdb855633d18f5777e5a81352e7bcce5eb0d27a4
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-03 12:09:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,13 +50,13 @@ def main():
     
     tab1, tab2, tab3, tab4 = st.tabs(["Floor Price", "Owners","Total Listed","Total Volume"])
     with tab1:
-        st.line_chart(fpcsv[["timestamp", "floor_price"]])
+        st.line_chart(fpcsv[["timestamp", "floor_price"]], x = 'timestamp')
     with tab2:
-        st.line_chart(fpcsv[["timestamp", "owners"]])
+        st.line_chart(fpcsv[["timestamp", "owners"]], x = 'timestamp')
     with tab3:
-        st.line_chart(fpcsv[["timestamp", ",total_listed"]])
+        st.line_chart(fpcsv[["timestamp", ",total_listed"]], x = 'timestamp')
     with tab4:
-        st.line_chart(fpcsv[["timestamp", "total_volume"]])
+        st.line_chart(fpcsv[["timestamp", "total_volume"]], x = 'timestamp')
     
     
     st.sidebar.image("https://cdn.discordapp.com/attachments/1117712065293987840/1124212987243278356/rpbp.png", use_column_width=True)
