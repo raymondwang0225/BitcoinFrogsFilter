a36aa3e0559d95624b2fac4bd91eafbc16fd40d4
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-03 12:06:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,19 +46,17 @@ def main():
     st.markdown("<hr/>", unsafe_allow_html = True)
 
     fpcsv = pd.read_csv('floor_price.csv', parse_dates=['timestamp'])
-    fpcsv2 = pd.read_csv('floor_price.csv', parse_dates=['timestamp'])
-    fpcsv3 = pd.read_csv('floor_price.csv', parse_dates=['timestamp'])
-    fpcsv4 = pd.read_csv('floor_price.csv', parse_dates=['timestamp'])
+    
     
     tab1, tab2, tab3, tab4 = st.tabs(["Floor Price", "Owners","Total Listed","Total Volume"])
     with tab1:
-        st.line_chart(fpcsv[["timestamp", "floor_price"]], x = 'timestamp')
+        st.line_chart(fpcsv[["timestamp", "floor_price"]])
     with tab2:
-        st.line_chart(fpcsv2[["timestamp", "owners"]], x = 'timestamp')
+        st.line_chart(fpcsv[["timestamp", "owners"]])
     with tab3:
-        st.line_chart(fpcsv3[["timestamp", ",total_listed"]], x = 'timestamp')
+        st.line_chart(fpcsv[["timestamp", ",total_listed"]])
     with tab4:
-        st.line_chart(fpcsv4[["timestamp", "total_volume"]], x = 'timestamp')
+        st.line_chart(fpcsv[["timestamp", "total_volume"]])
     
     
     st.sidebar.image("https://cdn.discordapp.com/attachments/1117712065293987840/1124212987243278356/rpbp.png", use_column_width=True)
